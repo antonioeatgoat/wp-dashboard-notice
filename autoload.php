c30f5c1e00ce7cf53bb301322e0c57c79abaf33e
antonioeatgoat <mangiacapra.antonio@gmail.com>
2018-08-01 22:25:22 +0200

    Init project with composer and autoloader files

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Autoload the classes of the plugin.
+ * At the current moment it autoload only the classes present in the ST_CLASSES_PATH folder, but not in ST_ADMIN_PATH
+ *
+ * @todo change doc and check if add a if file_exists before include
+ * @param  string $class
+ *
+ * @return bool
+ */
+function nmwp_autoload($class) {
+
+    if ( false !== strpos( $class, 'aeg_NM_' ) ) {
+        return false;
+    }
+
+    $directory_path = dirname(__FILE__) . '/src/';
+    $file_name      = str_replace('_', DIRECTORY_SEPARATOR, $class);
+
+    include $directory_path . $file_name;
+
+    return true;
+
+}
+
+spl_autoload_register('nmwp_autoload');
