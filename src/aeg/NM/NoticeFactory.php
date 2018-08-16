4a98c51d92872e418bcf9b394f442029fe333ba1
antonioeatgoat <mangiacapra.antonio@gmail.com>
2018-08-16 15:16:25 +0200

    Align factory with the latest editings (blank removal, unique identified added)

--- src/aeg/NM/NoticeFactory.php
+++ src/aeg/NM/NoticeFactory.php
@@ -2,11 +2,17 @@
 
 class aeg_NM_NoticeFactory {
 
-	public function create( $message, $args = array() ) {
-		return new aeg_NM_Notice( $message, $args );
+	/**
+	 * Creates a Notice object, see the documentation of the object to have more details on the parameters
+	 *
+	 * @param string $id
+	 * @param string $message
+	 * @param array  $args
+	 *
+	 * @return aeg_NM_Notice
+	 */
+	public function create( $id, $message, $args = array() ) {
+		return new aeg_NM_Notice( $id, $message, $args );
 	}
 
-	public function create_blank( $html_content ) {
-		return new aeg_NM_Notice( '', array( 'blank_content' => $html_content) );
-	}
 }
