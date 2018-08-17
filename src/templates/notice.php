3f8cd3cee46d8700a3196b1073691b3551cadee9
antonioeatgoat <mangiacapra.antonio@gmail.com>
2018-08-17 14:02:16 +0200

    Add all the attributes to the notice template

--- src/templates/notice.php
+++ src/templates/notice.php
@@ -7,8 +7,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * @var aeg_NM_Notice $notice
  */
+
+$css_clases = 'notice-' . $notice->get_status();
+$css_clases .= ' ' . $notice->get_custom_class();
+
+if ( $notice->get_show_close_btn() ) {
+	$css_clases .= ' is-dismissible';
+}
+
 ?>
 
-<div id="<?php echo esc_attr( $notice->get_id() ); ?>" class="aeg-notice-manaeger <?php echo $notice->get_custom_class(); ?>">
-	<p><?php echo $notice->get_message(); ?></p>
+<div id="<?php echo esc_attr( $notice->get_id() ); ?>"
+     class="aeg-notice notice <?php echo $css_clases; ?>">
+    <p><?php echo $notice->get_message(); ?></p>
+
+	<?php if ( $notice->get_cta_anchor() && $notice->get_cta_href() || $notice->get_dismiss_anchor() && 'none' !== $notice->get_dismiss_mode() ): ?>
+        <p class="aeg-notice__buttons">
+
+			<?php if ( $notice->get_cta_anchor() && $notice->get_cta_href() ): ?>
+                <a href="<?php echo $notice->get_cta_href() ?>"
+                   class="button-primary"><?php echo $notice->get_cta_anchor(); ?></a>
+			<?php endif; ?>
+			<?php if ( $notice->get_dismiss_anchor() && 'none' !== $notice->get_dismiss_mode() ): ?>
+                <a class="button button-link"
+                   href="todo link here"><?php echo $notice->get_dismiss_anchor(); ?></a>
+			<?php endif; ?>
+
+        </p>
+	<?php endif; ?>
+
+	<?php if ( $notice->get_show_close_btn() ): ?>
+        <button type="button" class="notice-dismiss"><span
+                    class="screen-reader-text"><?php _e( 'Dismiss this notice.' ); ?></span></button>
+	<?php endif; ?>
 </div>
