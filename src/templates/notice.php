f2c98d161be3b10c3194a34c7ac2226ca3e6b675
antonioeatgoat <mangiacapra.antonio@gmail.com>
2018-08-17 18:17:22 +0200

    Improve alignment of cta buttons in the notice message

--- src/templates/notice.php
+++ src/templates/notice.php
@@ -15,21 +15,24 @@ if ( $notice->get_show_close_btn() ) {
 	$css_clases .= ' is-dismissible';
 }
 
+$cta_button_exists     = ( $notice->get_cta_anchor() && $notice->get_cta_href() );
+$dismiss_button_exists = ( $notice->get_dismiss_anchor() && 'none' !== $notice->get_dismiss_mode() );
+$dismiss_button_css = ( $cta_button_exists ) ? 'button-cancel' : 'link-text';
 ?>
 
 <div id="<?php echo esc_attr( $notice->get_id() ); ?>"
      class="aeg-notice notice <?php echo $css_clases; ?>">
     <p><?php echo $notice->get_message(); ?></p>
 
-	<?php if ( $notice->get_cta_anchor() && $notice->get_cta_href() || $notice->get_dismiss_anchor() && 'none' !== $notice->get_dismiss_mode() ): ?>
+	<?php if ( $cta_button_exists || $dismiss_button_exists ): ?>
         <p class="aeg-notice__buttons">
 
-			<?php if ( $notice->get_cta_anchor() && $notice->get_cta_href() ): ?>
+			<?php if ( $cta_button_exists ): ?>
                 <a href="<?php echo $notice->get_cta_href() ?>"
                    class="button-primary"><?php echo $notice->get_cta_anchor(); ?></a>
 			<?php endif; ?>
-			<?php if ( $notice->get_dismiss_anchor() && 'none' !== $notice->get_dismiss_mode() ): ?>
-                <a class="button button-link"
+			<?php if ( $dismiss_button_exists ): ?>
+                <a class="<?php echo $dismiss_button_css; ?>"
                    href="<?php echo $notice->get_dismiss_url(); ?>"><?php echo $notice->get_dismiss_anchor(); ?></a>
 			<?php endif; ?>
 
