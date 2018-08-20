085451a8784ea8d9fc30b0ee100cf56dbb7f1648
antonioeatgoat <11173091+antonioeatgoat@users.noreply.github.com>
2018-08-20 23:14:43 +0200

    Initial commit

---
+++ src/aeg/NM/NoticeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class aeg_NM_NoticeFactory
+ */
+class aeg_NM_NoticeFactory {
+
+	/**
+	 * Creates a Notice object, see the documentation of the object to have more details on the parameters
+	 *
+	 * @param string $id
+	 * @param string $message
+	 * @param array  $args
+	 *
+	 * @return aeg_NM_Notice
+	 */
+	public function create( $id, $message, $args = array() ) {
+		return new aeg_NM_Notice( $id, $message, $args );
+	}
+
+}
